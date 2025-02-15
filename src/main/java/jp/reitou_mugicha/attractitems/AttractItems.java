799390e889ca68f5e1df82816ac15bdd65e8f4ec
ao-4x4 <kounoaoto1029@icloud.com>
2025-02-15 23:22:16 +0900

    First

---
+++ src/main/java/jp/reitou_mugicha/attractitems/AttractItems.java
@@ -0,0 +1,20 @@
+package jp.reitou_mugicha.attractitems;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AttractItems extends JavaPlugin
+{
+
+    @Override
+    public void onEnable()
+    {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+        // Plugin shutdown logic
+    }
+}
